ac5f4569657c2d0b9beda8371904db362e5676b0
choirinapriska <priskachoirina17@gmail.com>
2024-03-19 22:43:04 +0700

    V2 - Tambah class getQuadran

---
+++ getQuadran.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class getQuadran:
+    def __init__(self, coorData) :
+        self.dataA = coorData[:, 4]
+        self.dataB = coorData[:, 5]
+    
+    def setQuadran(self):
+        Quadran = np.empty((len(self.dataA), 6), dtype=object)
+
+        for i in range(len(self.dataA)):
+     
+            X = np.int_(self.dataA[i])
+            Y = np.int_(self.dataB[i])
+
+            tetha = np.degrees(np.arctan2(Y,X)) + 360*(Y<0)
+            magnitude = np.sqrt(np.power(X,2) + np.power(Y,2))
+
+            qdLabel = ''
+
+            if X != 0 and Y != 0:
+                if tetha >= 0 and tetha < 90:
+                    qdLabel = 'Q1'
+                elif tetha >= 90 and tetha < 180:
+                    qdLabel = 'Q2'
+                elif tetha >= 180 and tetha < 270 : 
+                    qdLabel = 'Q3'
+                elif tetha >= 270 and tetha < 360:
+                    qdLabel = 'Q4'
+                else :
+                    qdLabel = 'No Quadran'
+            else :
+                qdLabel = 'No Quadran'
+
+            Quadran[i, :] = [ np.str_(i) ,X, Y, tetha, magnitude, qdLabel]
+
+        return Quadran
+        
+         
+         
+         
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 
 from POC import POC as POC
 from getVektor import getVektor as Vect
+from getQuadran import getQuadran as Qd
 
 # inisiasi gambar
 imgBlockCur = cv2.cvtColor(cv2.imread('image/imgTitik/5-1.jpg'), cv2.COLOR_BGR2GRAY) 
@@ -23,8 +25,16 @@ plt.imshow(np.uint8((imgBlockCur*0.9) + (imgBlockRef*0.1)), cmap="gray")
 
 # pemanggilan class dan method untuk menampilkan quiver / gambar panah
 initQuiv = Vect(valPOC, blockSize)
-quiv = initQuiv.createVektor()
+quivData = initQuiv.createVektor()
 
-plt.quiver(quiv[:, 0], quiv[:, 1], quiv[:, 2], quiv[:, 3], scale=1, scale_units='xy', angles='xy', color="r", pivot='middle')    
+plt.quiver(quivData[:, 0], quivData[:, 1], quivData[:, 2], quivData[:, 3], scale=1, scale_units='xy', angles='xy', color="r", pivot='middle')    
+
+# Pemanggilan class untuk mengeluarkan nilai karakteristik vektor
+# blok ke, x,y,tetha, magnitude, dan quadran ke
+initQuadran = Qd(quivData)
+quadran = initQuadran.setQuadran()
+print(tabulate(quadran, headers=['Blok Ke', 'X', 'Y', 'Tetha', 'Magnitude', 'Quadran Ke']))
+
+
 plt.axis('on')  # Matikan sumbu
 plt.show()
